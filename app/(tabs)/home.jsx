da3bcbb9630f01cd74f9f628b76e9d739547d888
msrjr91 <michael.s.ross10@gmail.com>
2025-01-21 21:33:37 -0500

    set up layouts for tabs and added a notifications page

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -4,12 +4,11 @@ import { Link, router, Tabs } from 'expo-router'
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { icons } from '../../constants'
-import CustomButton from '../../components/CustomButton'
 
 const Home = () => {
   return (
     <SafeAreaView className="bg-primary h-full relative">
-        <Pressable onPress={() => router.push('/index')} className="absolute top-[10%] right-[5%]" style={{ zIndex: 10 }}>
+        <Pressable onPress={() => router.push('/notifications')} className="absolute top-[10%] right-[5%]" style={{ zIndex: 10 }}>
           <Image 
             source={icons.notification}
             className='w-6 h-6'
